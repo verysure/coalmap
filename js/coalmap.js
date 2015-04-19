65d8a83227a713b2283cf9b7653e3c2c411021a6
verysure <ttttonywu@gmail.com>
2015-04-18 20:08:51 -0400

    api from nrel

--- js/coalmap.js
+++ js/coalmap.js
@@ -105,7 +105,7 @@ $(document).ready(function() {
     var apikey = 'AyxJQCb3jgWo5pWvz122yF2SdWCcHGxviGgfa4Eo';
     $.ajax({
             type: 'GET',
-            url: 'https://developer.nrel.gov/api/pvwatts/v5.json?api_key=DEMO_KEY&lat=40&lon=-105&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10',
+            url: 'https://developer.nrel.gov/api/pvwatts/v5.json?api_key=' + apikey + '&lat=40&lon=-105&system_capacity=4&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10',
             dataType: "jsonp",
             crossDomain: true,
             success: function (msg) {
