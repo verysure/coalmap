a71ae7e1477c7d8e1c342003886ad4e65f007b91
verysure <ttttonywu@gmail.com>
2015-04-18 20:25:44 -0400

    api from nrel

--- js/coalmap.js
+++ js/coalmap.js
@@ -103,8 +103,8 @@ function getLCOE(position) {
     console.log('LCOE');
     $.ajax({
         type: 'GET',
-        // url: 'https://developer.nrel.gov/api/pvwatts/v5.json?api_key=' + apikey + '&lat=' + toString(position['lat']) +'&lon='+ toString(position['lng']) + '&system_capacity=20000&azimuth=180&tilt=40&array_type=4&module_type=0&losses=10',
-        url: 'https://developer.nrel.gov/api/pvwatts/v5.json?api_key=' + apikey + '&lat=40.5&lon=-105&system_capacity=20000&azimuth=180&tilt=40&array_type=4&module_type=0&losses=10',
+        url: 'https://developer.nrel.gov/api/pvwatts/v5.json?api_key=' + apikey + '&lat=' + toString(position['lat']) +'&lon='+ toString(position['lng']) + '&system_capacity=20000&azimuth=180&tilt=40&array_type=4&module_type=0&losses=10',
+        // url: 'https://developer.nrel.gov/api/pvwatts/v5.json?api_key=' + apikey + '&lat=40.5&lon=-105&system_capacity=20000&azimuth=180&tilt=40&array_type=4&module_type=0&losses=10',
         dataType: "jsonp",
         crossDomain: true,
         success: function (msg) {
