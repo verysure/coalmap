8409a780725ad5bc9429e7acd1fd83a573bbe388
verysure <ttttonywu@gmail.com>
2015-04-21 17:19:25 -0400

    fix co2

--- js/coalmap.js
+++ js/coalmap.js
@@ -96,7 +96,7 @@ function addCoalPlants(fdata) {
                 lat: raw_plant_data[i]['Latitude'],
                 lng: raw_plant_data[i]["Longitude"]
             },
-            icon: planticon(coal_mc, pv_lcoe, co2/20000000),
+            icon: planticon(coal_mc, pv_lcoe, raw_plant_data[i]["CO2"]/20000000),
             info: renderInfo({
                 title: title,
                 coal_mc: coal_mc.toFixed(2),
