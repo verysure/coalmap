05372b8c748495501bb90e2749a3f5736dd27d3a
verysure <ttttonywu@gmail.com>
2015-04-22 05:47:04 -0400

    fix chart; fix math.round

--- js/coalmap.js
+++ js/coalmap.js
@@ -209,7 +209,7 @@ function drawTimeLine(formdata) {
     }
     for (var i = 0; i < plant_data.length; i++) {
         year = Math.log(coalMarginalCost(plant_data[i], formdata)/pvLCOE(plant_data[i], formdata))/Math.log(1-formdata.solarred/100);
-        year_plants[round(year)-2015][1] += 1;
+        year_plants[Math.round(year)-2015][1] += 1;
     }
     for (var y = 2015; y < 2050; y++) {
         year_plants[y-2015+1][1]+=year_plants[y-2015][1]
