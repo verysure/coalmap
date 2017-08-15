7ba069274d33b3bbd4eaf5b5a06ead779cddba39
verysure <ttttonywu@gmail.com>
2017-08-14 23:35:15 -0400

    edit js path

--- js/coalmap.js
+++ js/coalmap.js
@@ -37,7 +37,8 @@ function updateMapGraph() {
     // addCoalPlants, check if there are plant_data
     if (plant_data.length === 0) {
         // parseJSON("/coalmap/data/alldata_records_unformatted.json", function() {
-        parseJSON("data/alldata_records.json", function() {
+        // parseJSON("data/alldata_records.json", function() {
+        parseJSON("/coalmap/data/alldata_records.json", function() {
             addCoalPlants(getFormData());
             drawTimeLine(getFormData());
             // drawScatteredChart(getFormData());
